790fdc3787113d08465288dc5cfbacb694b5878f
Francesco Mazzini <f.mazzini@reply.de>
2024-12-07 19:41:21 +0100

    Fixing issue about encoding
    Signed-off-by: Francesco Mazzini <f.mazzini@reply.de>

--- .github/scripts/compliance-review.js
+++ .github/scripts/compliance-review.js
@@ -116,6 +116,7 @@ main()
     console.log(message);
     console.log(`::set-output name=status::${result.status}`);
     console.log(`::set-output name=message::${message}`);
+    
 
     // let escapedMessage = result.message
     //   .replace(/%/g, '%25')    // Escape %
